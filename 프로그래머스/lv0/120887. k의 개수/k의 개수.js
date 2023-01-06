ed82d1f8166ea54564ca465abd2d7f8db3f82114
isol.bae <65801741+jjangi@users.noreply.github.com>
2023-01-06 21:54:38 +0900

    [level 0] Title: k의 개수, Time: 10.31 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -0,0 +1,26 @@
+function solution(i, j, k) {
+    let a = "";
+    for (i; i <= j; i++) {
+        a += i;
+    }
+
+    return a.split(k).length - 1;
+}
+
+function solution(i, j, k) {
+    return (
+        Array(j - i + 1)
+            .fill(i)
+            .map((v, i) => v + i)
+            .join("")
+            .split(k).length - 1
+    );
+}
+
+function solution(i, j, k) {
+    let str = Array(j - i + 1)
+        .fill(i)
+        .map((v, i) => v + i)
+        .join("");
+    return Array.from(str).filter((t) => +t === k).length;
+}
